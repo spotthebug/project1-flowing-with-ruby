5d8a41f476e0692a03c3167787f5e6d998b4ea5d
camario25 <camario25@yahoo.com>
2018-03-01 10:15:05 -0800

    added user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,30 @@
 class UsersController < ApplicationController
-
+  def index
+    
+  end
+  
   def new
+    @user = User.new
   end
-
+  
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+      flash[:notice] = "You successfully joined Flowing With Ruby"
+    else
+      flash[:error] = "There was problem, please try sign up again"
+    end
   end
-
+  
   def show
+    @user = User.find(params[:id])
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end
