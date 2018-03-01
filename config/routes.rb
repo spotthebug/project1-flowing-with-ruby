a7b9f021cb90d2ea83fffa5490a20aadf24d4c5b
camario25 <camario25@yahoo.com>
2018-03-01 10:54:20 -0800

    added session everything --need to test

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
 #         PATCH  /users/:id(.:format)      users#update
 #         PUT    /users/:id(.:format)      users#update
 #         DELETE /users/:id(.:format)      users#destroy
+get '/login', to: 'sessions#new'
+get '/logout', to: 'sessions#destroy'
+post '/sessions', to: 'sessions#create'
 end
