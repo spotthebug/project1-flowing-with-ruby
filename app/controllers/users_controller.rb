4fe3f196662f8cf547500440ba0d4323ddc28f80
camario25 <camario25@yahoo.com>
2018-03-01 11:25:29 -0800

    completed Sessions --need testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login(@user)
       redirect_to @user
       flash[:notice] = "You successfully joined Flowing With Ruby"
     else
